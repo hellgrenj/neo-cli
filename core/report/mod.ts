9a46323b90a5ff510c93000259162cc6cfb7aa16
hellgrenj <hellgrenj@gmail.com>
2020-06-28 23:06:19 +0200

    refactored report

--- core/report/mod.ts
+++ core/report/mod.ts
@@ -10,8 +10,18 @@ import {
   numberOfHazardousObjects,
   consideredPotentiallyHazardous,
 } from "../hazard/mod.ts";
+import { NearEarthObject } from "../_types/nearEarthObject.ts";
+import { pipe } from "../util/mod.ts";
 
-
+interface Report {
+    response: any;
+    closest: NearEarthObject;
+    closestDistanceInKm: number;
+    minDia: number;
+    maxDia: number;
+    kmPerSecond: number;
+    kmPerHour: number;
+  }
 /** 
 takes a response from the NEO WS API and returns a report
 @param {any} response - the API Response
@@ -20,11 +30,13 @@ takes a response from the NEO WS API and returns a report
 export const createReport = (
   response: any,
 ): string => {
+  return pipe(constructReport, formatReport)(response);
+};
+const constructReport = (response: any): Report => {
   const closest = closestToEarth(response.near_earth_objects);
 
   const closestDistanceInKm = Math.round(
-    closest.close_approach_data[0].miss_distance
-      .kilometers,
+    closest.close_approach_data[0].miss_distance.kilometers,
   );
   const [minDia, maxDia] = estimatedDiameterInMeters(
     closest,
@@ -32,43 +44,57 @@ export const createReport = (
   const [kmPerSecond, kmPerHour] = relativeVelocity(
     closest,
   );
-
+  return {
+    response,
+    closest,
+    closestDistanceInKm,
+    minDia,
+    maxDia,
+    kmPerSecond,
+    kmPerHour,
+  } as Report;
+};
+const formatReport = (
+  report: Report,
+): string => {
   return `In total ${
-    c.green(response.element_count.toString())
+    c.green(report.response.element_count.toString())
   } objects where found and ${
     c.red(
       numberOfHazardousObjects(
-        response.near_earth_objects,
+        report.response.near_earth_objects,
       ).toString(),
     )
   } where considered hazardous.
-        
+            
 The closest one passed just ${
     c.yellow(
-      formatNumberString(closestDistanceInKm),
+      formatNumberString(report.closestDistanceInKm),
     )
   } km from earth (${
     c.yellow(
       formatNumberString(kilometersToScandinavianMiles(
-        closestDistanceInKm,
+        report.closestDistanceInKm,
       )),
     )
   } scandinavian miles)
 thats like driving thru Sweden ${
-    c.green(timesThruSweden(closestDistanceInKm).toString())
+    c.green(timesThruSweden(report.closestDistanceInKm).toString())
   } times
-        
-its estimated to be between ${c.green(minDia.toString())} and ${
-    c.green(maxDia.toString())
+            
+its estimated to be between ${c.green(report.minDia.toString())} and ${
+    c.green(report.maxDia.toString())
   } meters in diameter travelling at a speed (relative to us) 
-of ${c.yellow(formatNumberString(kmPerSecond))} km per second (${
-    c.yellow(formatNumberString(kmPerHour))
+of ${c.yellow(formatNumberString(report.kmPerSecond))} km per second (${
+    c.yellow(formatNumberString(report.kmPerHour))
   } km per hour)
-        
+            
 this object (the closest one) is ${
-    consideredPotentiallyHazardous(closest) ? c.red("(!)") : c.green("NOT")
+    consideredPotentiallyHazardous(report.closest)
+      ? c.red("(!)")
+      : c.green("NOT")
   } considered potentially hazardous
-        `.trim();
+            `.trim();
 };
 const formatNumberString = (n: number): string => {
   return n.toString().replace(/(\d)(?=(\d{3})+$)/g, "$1 ");
